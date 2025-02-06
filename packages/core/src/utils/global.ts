911f3ce41c0fe1aab2ec5237a70c4597ba90e762
Anhofa <22050805@hdu.edu.cn>
2025-02-06 23:59:10 +0800

    配置初始化

---
+++ packages/core/src/utils/global.ts
@@ -0,0 +1,62 @@
+import { isWindow } from './is'
+import { WebTracing } from '../optionType'
+
+declare global {
+  interface Window {
+    __webTracingInit__?: boolean;
+    __webTracing__?: WebTracing;
+    process?: {
+      versions?: {
+        electron?: string;
+      };
+    };
+  }
+}
+
+
+/**
+ * 是否为浏览器环境
+ */
+export const isBrowserEnv = isWindow(typeof window !== 'undefined' ? window : 0)
+
+/**
+ * 是否为 electron 环境
+ */
+export const isElectronEnv = !!window?.process?.versions?.electron
+
+/**
+ * 是否为测试环境
+ */
+export const isTestEnv =
+  (typeof navigator !== 'undefined' && navigator.userAgent.includes('jsdom')) ||
+  // @ts-expect-error: jsdom
+  (typeof window !== 'undefined' && window.jsdom)
+
+/**
+ * 获取全局变量
+ */
+export function getGlobal(): Window {
+  if (isBrowserEnv || isElectronEnv || isTestEnv) return window
+  return {} as Window
+}
+
+/**
+ * 获取全部变量 __webTracing__ 的引用地址
+ */
+export function getGlobalSupport(): WebTracing {
+  _global.__webTracing__ = _global.__webTracing__ || ({} as WebTracing)
+  return _global.__webTracing__
+}
+
+/**
+ * 判断sdk是否初始化
+ * @returns sdk是否初始化
+ */
+export function isInit(): boolean {
+  return !!_global.__webTracingInit__
+}
+
+const _global = getGlobal()
+const _support = getGlobalSupport()
+
+export { _global, _support }
